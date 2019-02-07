a5e496bf50d65a60db198245ad7bd27b2723767d
Junior Martins <valter.martins@metasix.com.br>
2019-02-07 19:42:11 -0200

    JavaScript Basico 2

---
+++ index.js
@@ -0,0 +1,28 @@
+// var olaMundo = "Olá mundo";
+
+// console.log(olaMundo);
+
+// let a = 10;
+
+// const b = "10";
+
+// console.log(a ===b); // Quando se usa == ele inginora o tipo de dado
+
+
+
+////////////CONDIÇÃO////////////////////////
+
+//let cor = "amarelo";
+
+// if(cor === "verde"){
+//     console.log("siga");
+
+// }else if (cor === "amarelo"){
+//     console.log("atenção")
+
+// } else{
+//     console.log("pare");
+// }
+
+
+///////////////////////////////////////////////
